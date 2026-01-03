334f0a372719a79b643dac187df7e43b19ee21d6
tubsn <stuff@artmessengers.de>
2026-01-03 23:06:12 +0100

    fix darkmode in new js script

--- public/styles/js/main.js
+++ public/styles/js/main.js
@@ -303,3 +303,29 @@ methods: {
 
 
 }).mount('#chatapp')
+
+
+// Darkmode
+function toggleDarkmode() {
+
+	let cssLink = document.querySelector('#dark-mode-css-link')
+	
+	if (cssLink) {
+		cssLink.remove()
+		document.cookie = 'darkmode = 0; path=/; expires=Fri, 31 Dec 1970 23:59:59 GMT'
+		return
+	}
+
+	let link = document.createElement('link')
+	link.id = 'dark-mode-css-link'
+	link.rel = 'stylesheet'
+	link.type = 'text/css'
+	link.href = '/styles/css/darkmode.css'
+	document.getElementsByTagName("head")[0].appendChild(link)
+	document.cookie = 'darkmode = 1;path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT'
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	let colorModeIcon = document.querySelector('.color-mode')
+	if (colorModeIcon) {colorModeIcon.addEventListener('click', event => {toggleDarkmode()})}
+});
